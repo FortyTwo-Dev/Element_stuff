b1ba2a526c2fbb0dd6c82f5542e5858306e28a71
Titi0603 <91018423+Titi0603@users.noreply.github.com>
2022-05-28 20:58:35 +0200

    model armor 3D

--- src/main/java/fr/titi/elementstuff/init/Items/armor/custom/GemArmorItem.java
+++ src/main/java/fr/titi/elementstuff/init/Items/armor/custom/GemArmorItem.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.Item;
+import software.bernie.example.registry.ItemRegistry;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
 import software.bernie.geckolib3.core.builder.AnimationBuilder;
@@ -16,6 +17,10 @@ import software.bernie.geckolib3.core.manager.AnimationData;
 import software.bernie.geckolib3.core.manager.AnimationFactory;
 import software.bernie.geckolib3.item.GeoArmorItem;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GemArmorItem extends GeoArmorItem implements IAnimatable {
     private AnimationFactory factory = new AnimationFactory(this);
 
@@ -24,8 +29,24 @@ public class GemArmorItem extends GeoArmorItem implements IAnimatable {
     }
 
     private <P extends IAnimatable> PlayState predicate(AnimationEvent<P> event) {
+        LivingEntity livingEntity = event.getExtraDataOfType(LivingEntity.class).get(0);
         event.getController().setAnimation(new AnimationBuilder().addAnimation("idle", true));
-        return PlayState.CONTINUE;
+        if (livingEntity instanceof ArmorStandEntity) {
+            return PlayState.CONTINUE;
+        }else if (livingEntity instanceof PlayerEntity) {
+            PlayerEntity player = (PlayerEntity) livingEntity;
+
+            List<Item> equipmentList = new ArrayList<>();
+            player.getAllSlots().forEach((x) -> equipmentList.add(x.getItem()));
+
+            List<Item> armorList = equipmentList.subList(2, 6);
+
+            boolean isWearingAll = armorList
+                    .containsAll(Arrays.asList(ModItems.GEM_BOOTS.get(), ModItems.GEM_LEGGINGS.get(),
+                            ModItems.GEM_CHESTPLATE.get(), ModItems.GEM_HELMET.get()));
+            return isWearingAll ? PlayState.CONTINUE : PlayState.STOP;
+        }
+        return PlayState.STOP;
     }
     @Override
     public void registerControllers(AnimationData data) {
